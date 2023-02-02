97b5e0ddf6487f8a331edf40ba8ee17e586d2511
ZanaGuda <zanaguda28@gmail.com>
2023-02-02 13:44:03 +0100

    Two-way binding

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const clickedHandler = () => {
+    setTitle("Updated");
+  };
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickedHandler}>Change title</button>
+    </Card>
+  );
+};
+export default ExpenseItem;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+const NewExpense = (props) => {
+  const saveExpenseData = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseData}></ExpenseForm>
+    </div>
+  );
+};
+export default NewExpense;
