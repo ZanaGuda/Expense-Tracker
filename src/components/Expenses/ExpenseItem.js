54cdbf418decf12fafabbc622cc7275a67ca51a3
ZanaGuda <zanaguda28@gmail.com>
2023-02-06 12:46:35 +0100

    adding a chart bar

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickedHandler = () => {
-    setTitle("Updated");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickedHandler}>Change title</button>
     </Card>
   );
 };
